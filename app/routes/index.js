2308270f7b6358c97d47a18a9f171f67ba74224d
superlou <lousimons@gmail.com>
2018-11-10 20:57:39 -0500

    Broken file upload

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject } from '@ember/service';
+import { task } from 'ember-concurrency';
 
 export default Route.extend({
   player: inject(),
@@ -11,6 +12,22 @@ export default Route.extend({
   actions: {
     play(source) {
       this.get('player').play(source);
+    },
+
+    uploadMedia(file) {
+      this.get('uploadMediaFile').perform(file);
     }
-  }
+  },
+
+  uploadMedia(file) {
+    console.log('test');
+  },
+
+  uploadMediaFile: task(function * (file) {
+    try {
+      let response = yield file.upload('http://loupi1:8910/media/upload');
+    } catch (e) {
+      console.log('Error uploading')
+    }
+  }).maxConcurrency(3).enqueue(),
 });
