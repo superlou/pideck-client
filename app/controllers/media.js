835c26baba0a24ad804a347523b6a4050b427835
superlou <lousimons@gmail.com>
2019-07-02 13:03:40 -0400

    Sort files by name

--- app/controllers/media.js
+++ app/controllers/media.js
@@ -1,6 +1,10 @@
 import Controller from '@ember/controller';
 import { inject } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
-  player: inject()
+  player: inject(),
+  sortedFiles: computed('model.[]', function() {
+    return this.get('model').sortBy('source');
+  }),
 });
