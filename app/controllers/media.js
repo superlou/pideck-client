7761b47f36f25036ec87c935b05cc8d5d5477e40
superlou <lousimons@gmail.com>
2019-07-02 13:24:30 -0400

    Split file listings into video and audio

--- app/controllers/media.js
+++ app/controllers/media.js
@@ -2,9 +2,22 @@ import Controller from '@ember/controller';
 import { inject } from '@ember/service';
 import { computed } from '@ember/object';
 
+function filename_is_audio(filename) {
+  let ext = filename.split('.').pop().toLowerCase();
+  return ['mp3', 'wav', 'aif', 'mid', 'ogg', 'aac', 'wma', 'flac'].includes(ext);
+}
+
 export default Controller.extend({
   player: inject(),
   sortedFiles: computed('model.[]', function() {
     return this.get('model').sortBy('source');
+  }),
+
+  music: computed.filter('sortedFiles.[]', function(file) {
+    return filename_is_audio(file.get('source'));
+  }),
+
+  video: computed.filter('sortedFiles.[]', function(file) {
+    return !filename_is_audio(file.get('source'));
   }),
 });
